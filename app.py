017680813e548b8f192446a75b7b138aab3ac843
jlostaln <136805206+jlostaln@users.noreply.github.com>
2025-11-07 15:28:19 +0200

    Add registration/login/logout

--- app.py
+++ app.py
@@ -1,7 +1,66 @@
+import sqlite3
 from flask import Flask
+from flask import render_template, request, redirect, session 
+from werkzeug.security import generate_password_hash, check_password_hash
+import config
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "Tämä on Pokemon-app!"
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        results = db.query(sql, [username])
+
+        if results:
+            result = results[0]
+            user_id = result["id"]
+            password_hash = result["password_hash"]
+
+            if check_password_hash(password_hash, password):
+                session["user_id"] = user_id
+                session["username"] = username
+                return redirect("/")
+            else:
+                return "VIRHE: väärä tunnus tai salasana"
+        else:
+            return "VIRHE: käyttäjätunnusta ei löytynyt"
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    del session["username"]
+    return redirect("/")
